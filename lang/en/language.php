fd63ae92f278cf6ad4cdf7716acd5fe0abc712b5
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:37:24 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,31 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/blocks/module.php
+
+$LANG['Blocks has been sorted'] = 'Blocks has been sorted';
+$LANG['Delete block'] = 'Delete block';
+$LANG['Delete'] = 'Delete';
+$LANG['Edit block'] = 'Edit block';
+$LANG['Add block'] = 'Add block';
+$LANG['Title'] = 'Title';
+$LANG['Content'] = 'Content';
+$LANG['Display title'] = 'Display title';
+$LANG['Submit'] = 'Submit';
+$LANG['Insert a title'] = 'Insert a title';
+$LANG['No block content'] = 'No block content';
+$LANG['Edit'] = 'Edit';
+// Translation of file modules/blocks/install.inc
+
+$LANG['Blocks'] = 'Blocks';
+// Translation of file modules/blocks/menu.inc
+
+$LANG['Sort blocks'] = 'Sort blocks';
+$LANG['View blocks'] = 'View blocks';
+$LANG['Add block'] = 'Add block';
+// Translation of file modules/blocks/custom/module.php
+
+$LANG['Block has been added'] = 'Block has been added';
+$LANG['Block has been deleted'] = 'Block has been deleted';
+$LANG['Block has been inserted'] = 'Block has been inserted';
