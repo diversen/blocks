4372bcab4560004561e8e7de75faaa2a812f70b7
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-07 18:45:10 +0100

    lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,20 +2,11 @@
 
 $LANG = array();
 
-$LANG['blocks'] = 'Blokke';
-$LANG['blocks_unused'] = 'Ubrugte blokke';
-$LANG['blocks_sec'] = 'Sekundære blokke ';
-$LANG['Blocks'] = 'Blokke';
-$LANG['Block has been added'] = 'Blok er blevet tilføjet';
-$LANG['Block has been deleted'] = 'Blok er slettet';
-$LANG['Block has been inserted'] = 'Blok er blevet indsat';
-$LANG['Sort blocks'] = 'Sorter blokke';
-$LANG['View blocks'] = 'Se blokke';
-$LANG['Add block'] = 'Tilføj blok';
 $LANG['Blocks has been sorted'] = 'Blokke er blevet sorteret';
 $LANG['Delete block'] = 'Slet blok';
 $LANG['Delete'] = 'Slet';
 $LANG['Edit block'] = 'Ret blok';
+$LANG['Add block'] = 'Tilføj blok';
 $LANG['Title'] = 'Titel';
 $LANG['Content'] = 'Indhold';
 $LANG['Display title'] = 'Vist title';
@@ -23,3 +14,12 @@ $LANG['Submit'] = 'Indsend';
 $LANG['Insert a title'] = 'Indsæt en titel';
 $LANG['No block content'] = 'Ingen blok indhold';
 $LANG['Edit'] = 'Ret';
+$LANG['blocks'] = 'Blokke';
+$LANG['blocks_unused'] = 'Ubrugte blokke';
+$LANG['blocks_sec'] = 'Sekundære blokke ';
+$LANG['Blocks'] = 'Blokke';
+$LANG['Sort blocks'] = 'Sorter blokke';
+$LANG['View blocks'] = 'Se blokke';
+$LANG['Block has been added'] = 'Blok er blevet tilføjet';
+$LANG['Block has been deleted'] = 'Blok er slettet';
+$LANG['Block has been inserted'] = 'Blok er blevet indsat';
